0cc357056ed8ea541d03a32608bc9acdb028ac3b
15cm <zhuhaitong15@gmail.com>
2015-07-17 16:14:28 +0800

    'music.h' modified

--- src/music.h
+++ src/music.h
@@ -8,10 +8,19 @@ class Music
 private:
     QString name;
     qint64 duration;
+    QString artist;
+    QString album;
     QUrl mp3Url;
 public:
     Music(){}
-    Music(QString _name, qint64 _duration, QUrl _url):name(_name),duration(_duration),mp3Url(_url){}
+    Music(QString _name, qint64 _duration, QUrl _url, QString _artist = QString("Not Found"), QString _album = QString("Not Found"))
+        :name(_name),
+         duration(_duration),
+         mp3Url(_url),
+         artist(_artist),
+         album(_album)
+    {
+    }
     QString GetName() const{return name;}
     qint64 GetDuration() const{return duration;}
     QString GetDurationStr () const
@@ -23,6 +32,8 @@ public:
         return QString(min.append(":").append(sec));
     }
     QUrl GetMp3Url() const{return mp3Url;}
+    QString GetArtist() const{return artist;}
+    QString GetAlbum() const{return album;}
     virtual ~Music(){}
 };
 
