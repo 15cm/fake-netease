ad75ae3256582b71817e672d08a5877bb82cae9a
hedejing <hedejin95@gmail.com>
2015-07-17 22:14:00 +0800

    constructor of OffMusic

--- src/local/OffMusic.h
+++ src/local/OffMusic.h
@@ -14,11 +14,11 @@ public:
 OffMusic::OffMusic(const QUrl &url)
 {
     TagLib::FileRef ref(url.path().toLatin1().data());
-    name = QString::fromStdString(ref.tag()->title().to8Bit());
     if(!ref.tag())
     {
         qDebug() << "NULL";
     }
+    name = QString(ref.tag()->title().toCString());
     duration = ref.audioProperties()->length();
     mp3Url = url;
     artist = QString(ref.tag()->artist().toCString());
