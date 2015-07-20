00a9efad48b3716dbabfb002343adf2672f92458
15cm <zhuhaitong15@gmail.com>
2015-07-20 21:06:52 +0800

    local music folder interface#

--- src/commander/implementation/selected_offmusic_deleter.h
+++ src/commander/implementation/selected_offmusic_deleter.h
@@ -2,11 +2,14 @@
 #define SELECTED_OFFMUSIC_DELETER
 
 #include "i_delete_selected_offmusic.h"
+#include "player.h"
 class SelectedOffMusicDeleter : public IDelteSelectedOffMusic
 {
 public:
-    void DeleteSelectedOffMusic()
+    void DeleteSelectedOffMusic(int index)
     {
+        Player list;
+        list.DeleteFromList(index);
     }
 };
 
