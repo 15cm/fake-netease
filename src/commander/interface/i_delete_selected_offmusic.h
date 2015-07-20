01d0f48e88bcc31805ac091917e6d86c81899490
15cm <zhuhaitong15@gmail.com>
2015-07-20 17:49:52 +0800

    interface add

--- src/commander/interface/i_delete_selected_offmusic.h
+++ src/commander/interface/i_delete_selected_offmusic.h
@@ -1,5 +1,11 @@
 #ifndef I_DELETE_SELECTED_OFFMUSIC
 #define I_DELETE_SELECTED_OFFMUSIC
 
+class IDelteSelectedOffMusic
+{
+public:
+    virtual void DeleteSelectedOffMusic(int index ) = 0;
+};
+
 #endif // I_DELETE_SELECTED_OFFMUSIC
 
