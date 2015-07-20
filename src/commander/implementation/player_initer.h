33cb77c5d743bc35c129d71e25510e40ec368a55
15cm <zhuhaitong15@gmail.com>
2015-07-20 15:16:56 +0800

    table init2

--- src/commander/implementation/player_initer.h
+++ src/commander/implementation/player_initer.h
@@ -9,7 +9,6 @@ public:
     void PlayerInit(QTableWidget *pTable)
     {
         Player list;
-<<<<<<< HEAD
         QVector<OffMusic> offList;
         list.Initialize(offList);
         for(int row = 0; row < offList.size(); row ++)
@@ -29,9 +28,7 @@ public:
                     break;
                 }
             }
-=======
         //list.Initialize();
->>>>>>> 2ccf83ea93cdae90f9f078fa0a4ec146bbce21f1
     }
 };
 #endif // PLAYER_INIT
